a3b9d1ed389f2bd9e34564862a838a5a99ab5f22
Matias-Alessandrello <matialess@gmail.com>
2024-02-26 00:05:49 -0300

    step 21

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let camperbot;
-let xp;
-console.log("Hello World");
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
